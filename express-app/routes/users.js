599d3a8341341cf4da476e8dc8ca40bea35961fd
Илья Котов <roskets801@gmail.com>
2026-03-24 19:01:13 +0500

    1-6

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
-
+let users = [];
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+router.get('/', function (req, res, next) {
+    res.send({
+        "id": 1,
+        "name": "name"
+    });
+});
+router.post('/', function (req, res, next) {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name
+    };
+    users.push(newUser);
+    res.status(201).json(newUser);
+})
+router.get('/:id', function (req, res, next) {
+    const userId = parseInt(req.params.id);
+    const user = users.find(u => u.id === userId);
+
+    if (!user) {
+        return res.status(404).json({error: 'User not found'});
+    }
+
+    res.json(user);
 });
 
 module.exports = router;
